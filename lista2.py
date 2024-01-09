72ec06b6b2426f5e5a10b8ab8e868bdf056d62a7
Raul Andrade <raul.sandrade@hotmail.com>
2024-01-09 20:30:20 -0300

    Resolução de alguns exercicíos

--- lista2.py
+++ lista2.py
@@ -95,8 +95,86 @@ def quadrado_numeros():
     
     print(lista_numeros_quadrado)
 
+def vetores_intercalados():
+    vetor1 = []
+    vetor2 = []
+    vetor3 = []
 
+    for i in range(0, 10):
+        numero = int(input("Digite um número"))
+        vetor1.append(numero)
+
+    for i in range(0, 10):
+        numero = int(input("Digite um número"))
+        vetor2.append(numero)
+
+    for i in range(0, 10):
+        vetor3.append(vetor1[i])
+        vetor3.append(vetor2[i])
+
+    print(vetor3)
+
+def vetores_intercalados3():
+    vetor1 = []
+    vetor2 = []
+    vetor3 = []
+    vetor4 = []
+
+    for i in range(0, 10):
+        numero = int(input("Digite um número"))
+        vetor1.append(numero)
+
+    for i in range(0, 10):
+        numero = int(input("Digite um número"))
+        vetor2.append(numero)
+
+    for i in range(0, 10):
+        numero = int(input("Digite um número"))
+        vetor3.append(numero)
+
+    for i in range(0, 10):
+        vetor4.append(vetor1[i])
+        vetor4.append(vetor2[i])
+        vetor4.append(vetor3[i])
+
+    print(vetor4)
+
+def altura_idade():
+    lista_altura = []
+    lista_idade = []
+    lista_medias = []
+
+
+    for i in range(0, 5):
+        altura = float(input("Digite a altura "))
+        idade = int(input("Digite a idade"))
+        lista_altura.append(altura)
+        lista_idade.append(idade)
+    
+    media_altura = sum(lista_altura) / len(lista_altura)
+    for i in range(0, len(lista_altura)):
+        if lista_altura[i] <= media_altura and lista_idade[i] >=13:
+            medias = lista_altura[i]
+            lista_medias.append(medias)
+    print("Numero de alunos com +13 que estão abaixo da média: {}" .format(len(lista_medias)))
+
+def temperaturas():
+    lista_temperatura = [27, 26, 26, 24, 23, 22, 20, 18, 20, 22, 24, 25]
+    lista_mes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    lista_meses_media_temp = []
+    media = sum(lista_temperatura) / len(lista_temperatura)
+    print(media)
+
+    for i in range(0, len(lista_temperatura)):
+        if lista_temperatura[i] >= media:
+            media_temp = lista_mes[i]
+            lista_meses_media_temp.append(media_temp)
+    
+    for i in range(1, len(lista_meses_media_temp) + 1):
+            print("{} {}" .format(i, lista_meses_media_temp[i]))
+
+    
 
 if (__name__ == "__main__"):
-    quadrado_numeros()
+    temperaturas()
     
